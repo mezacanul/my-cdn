9f2de5d62b286849c989926a5d656fc48f129773
Eduardo Meza <joseduardomezacanul@hotmail.com>
2026-03-27 21:12:00 -0600

    Feat: Added reponse mail to client before notif to personal email

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -4,16 +4,25 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { name, email, message } = body;
+  const EMAIL_TO = process.env.EMAIL_TO;
 
+  const clientEmail = email;
   const subject = `New Message from ${name}`;
-  const messageBody = `Reply to: ${email}\n\nMessage: ${message}`;
-  const to = email;
+  const messageBody = `Reply to: ${clientEmail}\n\nMessage: ${message}`;
+
+  const clientResponse = await sendEmail({
+    subject: `Hola ${name}!`,
+    message:
+      "Gracias por tu mensaje. Me pondré en contacto contigo lo antes posible.",
+    to: clientEmail,
+  });
+  console.log(clientResponse);
 
   // await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await sendEmail({
     subject: subject,
     message: messageBody,
-    to: email,
+    to: EMAIL_TO as string,
   });
   console.log(response);
 
